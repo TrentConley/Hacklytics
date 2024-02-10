08790eb9ca7e3f35622b46c37e6dbb09d080027d
Trent Conley <trentconley@gmail.com>
2024-02-10 15:59:50 -0500

    added firebase uploader

---
+++ frontend/firebase-upload-image/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import 'antd/dist/antd.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ frontend/firebase-upload-image/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Card } from 'antd'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className="container mt-5">
+      <Card>
+        <h2>Netlify auto build when push to main branch github</h2>
+      </Card>
+    </div>
+  )
+}
+
+export default HomePage
