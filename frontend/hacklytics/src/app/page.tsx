b596fd5fc15aec701252d24a4e234317e986ad1c
Trent Conley <trentconley@gmail.com>
2024-02-10 12:50:12 -0500

    creating components

---
+++ frontend/hacklytics/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ImageUpload from "../components/image-upload";
+
+export default function Home() {
+  return (
+    <div>
+      <ImageUpload />
+    </div>
+  );
+}
