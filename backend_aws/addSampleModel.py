b5999d3aeb48c8a814a4900a31adbec5099a529c
Mysterium422 <mysterium422@gmail.com>
2024-02-11 09:26:31 -0500

    Cleans up backend aws

--- backend_aws/addSampleModel.py
+++ backend_aws/addSampleModel.py
@@ -1,5 +1,5 @@
 import requests
 url = "http://18.188.69.104:5000"
 
-res = requests.post(url + '/upload/testname', json={"mytext":"lalala"})
+res = requests.post(url + '/upload/testname', json={"mytext":"test"})
 print(res)
